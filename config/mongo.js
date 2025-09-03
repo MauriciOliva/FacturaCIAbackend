5f44149322b415bd8f5b63ca106d15dd7be5f414
MauriciOliva <hectoroliva841@gmal.com>
2025-09-03 11:31:53 -0600

    Cambio de mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,33 +1,35 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 export const connect = async () => {
     try {
-        mongoose.connection.on('error', () => {
-            console.log('MongoDB | Could not be connect to mongodb')
+        mongoose.connection.on('error', ()=>{
+            console.log('MongoDB | Could not be connect to mongoDB');
         })
-        mongoose.connection.on('connecting', () => {
-            console.log('MongoDB | Try connecting')
+        mongoose.connection.on('connecting', ()=>{
+            console.log('MongoDB | try connecting');
         })
-        mongoose.connection.on('connected', () => {
-            console.log("MongoDB | Connected to MongoDB")
+        mongoose.connection.on('connected', ()=>{
+            console.log('MongoDB | connected to mongodb');
         })
-        mongoose.connection.once('open', () => {
-            console.log('MongoDB | Connected to database')
+        mongoose.connection.once('opne', ()=>{
+            console.log('MongoDB | connected to database');
         })
-        mongoose.connection.on('reconnected', () => {
-            console.log('MongoDB | Reconnected to mongodb')
+        mongoose.connection.on('reconnected', ()=>{
+            console.log('MongoDB | reconnected to mongoDB');
         })
-        mongoose.connection.on('disconnected', () => {
-            console.log('MongoDB | Disconnected')
+        mongoose.connection.on('disconnected', ()=>{
+            console.log('MongoDB | disconnected ');
         })
+
+        await mongoose.connect(
+            `${process.env.DB_SERVICE}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+            {
+                maxPoolSize: 50,
+                serverSelectionTimeoutMS: 5000
+            }//mongodb://127.0.0.1:27017/mydb
+        )
+    } catch (error) {
+        console.log('data base error', error);
         
-        // ✅ CONEXIÓN CORREGIDA - Usa MONGO_URI directamente
-        await mongoose.connect(`${process.env.DB_SERVICES}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, 
-        {
-            maxPoolSize: 50,
-            serverSelectionTimeoutMS: 5000,
-        })
-    } catch (err) {
-        console.error('Database connection failed', err)
     }
 }
